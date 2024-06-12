c3c9ae2bf97ba037477494408e61ce30fe51cd8a
mk <pinch24@gmail.com>
2024-06-12 10:09:52 +0900

    TRPL - 1. Hello, world!

---
+++ Sites/The Rust Programming Language/variables/target/debug/deps/variables-97d60be30e5f54f8.d
@@ -0,0 +1,5 @@
+/Users/mk/Studies/learn.rust/Sites/The Rust Programming Language/variables/target/debug/deps/libvariables-97d60be30e5f54f8.rmeta: src/main.rs
+
+/Users/mk/Studies/learn.rust/Sites/The Rust Programming Language/variables/target/debug/deps/variables-97d60be30e5f54f8.d: src/main.rs
+
+src/main.rs:
