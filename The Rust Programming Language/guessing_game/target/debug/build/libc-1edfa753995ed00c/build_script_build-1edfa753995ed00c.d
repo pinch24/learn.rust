4f03fae9dbd6ab7d0e12ff76471a7c64754599ba
pinch24 <pinch24@gmail.com>
2024-08-20 14:59:37 +0900

    Rename Folders

--- The Rust Programming Language/guessing_game/target/debug/build/libc-1edfa753995ed00c/build_script_build-1edfa753995ed00c.d
+++
@@ -1,5 +0,0 @@
-/Users/msk/Studies/learn.rust/The Rust Programming Language/guessing_game/target/debug/build/libc-1edfa753995ed00c/build_script_build-1edfa753995ed00c: /Users/msk/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.155/build.rs
-
-/Users/msk/Studies/learn.rust/The Rust Programming Language/guessing_game/target/debug/build/libc-1edfa753995ed00c/build_script_build-1edfa753995ed00c.d: /Users/msk/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.155/build.rs
-
-/Users/msk/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.155/build.rs:
--- The Rust Programming Language/variables/target/debug/deps/variables-fa0281046cf458b7.d
+++
@@ -1,5 +0,0 @@
-/Users/msk/Studies/learn.rust/The Rust Programming Language/variables/target/debug/deps/variables-fa0281046cf458b7: src/main.rs
-
-/Users/msk/Studies/learn.rust/The Rust Programming Language/variables/target/debug/deps/variables-fa0281046cf458b7.d: src/main.rs
-
-src/main.rs:
