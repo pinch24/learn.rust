a77d72d0fce89c5ac57cee37a5a0bd15df97a9ad
pinch24 <pinch24@gmail.com>
2024-09-04 13:12:37 +0900

    4.1 Owner

--- The Rust Programming Language/04.01 owner/src/main.rs
+++ The Rust Programming Language/04.01 owner/src/main.rs
@@ -37,6 +37,50 @@ fn main() {
 
     // 소유권과 함수
     let s = String::from("hello");
-
+    takes_ownership(s);
+    //println!("{}", s);  // Error: borrow of moved value: `s`
+    let x = 5;
+    makes_copy(x);
+    println!("{}", x);
     println!("--------------------------------");
+
+    // 반환 값과 스코프 - 1
+    let s1 = gives_ownership();
+    println!("{}", s1);
+
+    let s2 = String::from("hello");
+    println!("{}", s2);
+
+    let s3 = takes_and_gives_back(s2);
+    println!("{}", s3);
+    //println!("{}", s2); // Error: borrow of moved value: `s2`
+    println!("--------------------------------");
+
+    // 반환 값과 스코프 - 2
+    let s1 = String::from("hello");
+    let (s2, len) = calculate_length(s1);
+    println!("The length of '{}' is {}.", s2, len);
+    println!("--------------------------------");
+}
+
+fn takes_ownership(some_string: String) {
+    println!("{}", some_string);
+}
+
+fn makes_copy(some_integer: i32) {
+    println!("{}", some_integer);
+}
+
+fn gives_ownership() -> String {
+    let some_string = String::from("yours");
+    some_string
+}
+
+fn takes_and_gives_back(a_string: String) -> String {
+    a_string
+}
+
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len();
+    (s, length)
 }
